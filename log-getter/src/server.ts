fb7c9f3626564478f20f586dba18f6afa20d48bf
lisyoen <lisyoen@gmail.com>
2025-04-22 19:52:10 +0900

    Typescript 로 변경경

--- log-getter/src/server.ts
+++ log-getter/src/server.ts
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { Pool, PoolClient, QueryResult } from 'pg';
 
 const app = express();
 const cors = require('cors');
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // PostgreSQL 연결 설정
-const pool = new Pool({
+const pool: Pool = new Pool({
   user: 'log-getter',
   host: 'log-db',
   database: 'log_db',
@@ -17,16 +17,16 @@ const pool = new Pool({
 });
 
 // welcome 메시지
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Log Getter API!');
 });
 
 // POST 요청 처리
-app.post('/data', async (req, res) => {
-  const { sender, content } = req.body;
+app.post('/data', async (req: Request, res: Response) => {
+  const { sender, content }: { sender: string, content: string } = req.body;
 
   try {
-    const result = await pool.query(
+    const result: QueryResult = await pool.query(
       'INSERT INTO log_table (sender, content) VALUES ($1, $2) RETURNING *',
       [sender, content]
     );
@@ -38,7 +38,7 @@ app.post('/data', async (req, res) => {
 });
 
 // 서버 시작
-const PORT = process.env.PORT || 3000;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
